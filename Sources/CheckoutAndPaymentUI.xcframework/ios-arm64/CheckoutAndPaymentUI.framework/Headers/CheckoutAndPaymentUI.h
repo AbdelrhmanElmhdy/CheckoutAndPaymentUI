d6b64ccbca4cb1c1a5b9ac7d8f49b9ee4b5c6fdc
Abdelrhman Elmahdy <Abdelrahman.elmahdy@btech.com>
2023-07-30 11:57:00 +0300

    Add new binary

---
+++ Sources/CheckoutAndPaymentUI.xcframework/ios-arm64/CheckoutAndPaymentUI.framework/Headers/CheckoutAndPaymentUI.h
@@ -0,0 +1,18 @@
+//
+//  CheckoutAndPaymentUI.h
+//  CheckoutAndPaymentUI
+//
+//  Created by Abdelrhman Elmahdy on 24/07/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CheckoutAndPaymentUI.
+FOUNDATION_EXPORT double CheckoutAndPaymentUIVersionNumber;
+
+//! Project version string for CheckoutAndPaymentUI.
+FOUNDATION_EXPORT const unsigned char CheckoutAndPaymentUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CheckoutAndPaymentUI/PublicHeader.h>
+
+
